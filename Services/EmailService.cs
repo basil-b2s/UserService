25e0265500e2f725e838a6bff97159035a40b83c
Basil Saji <62460730+basil-b2s@users.noreply.github.com>
2024-02-14 15:01:53 +0530

    created email services

---
+++ Services/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Services
+{
+    public class EmailService : INotificationService
+    {
+        public void SendNotification(string Action, User user)
+        {
+            Console.WriteLine($"Email Service : User (id : {user.Id}) {Action} ");
+        }
+    }
+}
