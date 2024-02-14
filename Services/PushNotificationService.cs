1d10dd4751fdde66ddc251c7cbf93f531d0a6e7a
Basil Saji <62460730+basil-b2s@users.noreply.github.com>
2024-02-14 15:01:43 +0530

    created pushnotification service

---
+++ Services/PushNotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Services
+{
+    public class PushNotificationService : INotificationService
+    {
+        public void SendNotification(string Action, User user)
+        {
+            Console.WriteLine($"Push Notificatoin Service : User (id : {user.Id}) {Action} ");
+        }
+    }
+}
